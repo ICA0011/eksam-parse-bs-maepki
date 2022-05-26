70711f9039ed112ee8fac2ad74a9ac14b03679bd
Magnus <maepki@ttu.ee>
2022-05-26 12:35:32 +0300

    parse

--- parse_xml.py
+++ parse_xml.py
@@ -1,12 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
-import lxml
+
 
 def parse_xml():
   url = "http://upload.itcollege.ee/~aleksei/test.xml"
   xml_content = requests.get(url).content
   soup = BeautifulSoup(xml_content,'xml')
-  titles = soup.find_all('data')
   result = soup.find("data").get_text().strip()
 
   return result
